bcf41efea717802aef73680525b7283e02ee52e1
Franck Delache <franck.delache@shopify.com>
2018-11-28 10:17:05 -0500

    Add a title and a navigation

--- test/site_pages_test.rb
+++ test/site_pages_test.rb
@@ -7,21 +7,29 @@ class PalindromeAppTest < Minitest::Test
     Sinatra::Application
   end
 
+  def setup
+    @base_title = "Learn Enough Ruby Sample App"
+  end
+
   def test_index
     get '/'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    assert_equal "#{@base_title} | Home", doc(last_response).at_css('title').content
+    assert doc(last_response).at_css('nav')
   end
 
   def test_about
     get '/about'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    assert_equal "#{@base_title} | About", doc(last_response).at_css('title').content
   end
 
   def test_palindrome
     get '/palindrome'
     assert last_response.ok?
     assert doc(last_response).at_css('h1')
+    assert_equal "#{@base_title} | Palindrome Detector", doc(last_response).at_css('title').content
   end
 end
